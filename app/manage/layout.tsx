db91d45f8c846cc0be2e77c9a3f1982895f4ad85
zWhiteTiger <nattawut.whitetiger@gmail.com>
2026-06-09 02:42:20 +0700

    [DEBUG] Middleware

--- app/manage/layout.tsx
+++ app/manage/layout.tsx
@@ -1,12 +1,32 @@
 import Appbar from "@/components/appbar";
 import Footer from "@/components/footer";
 import { RadiantNoiseBackground } from "@/components/RadiantNoiseBackground";
+import { getServerSession } from "next-auth";
+import { authConfig } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
-export default function layout({
+const validRoles = [
+    "FLOORSTAFF",
+    "TEAMLEADER",
+    "MANAGER",
+];
+
+export default async function layout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+
+    const session = await getServerSession(authConfig);
+
+    if (!session) {
+        redirect("/login");
+    }
+
+    if (!validRoles.includes(session.user.role)) {
+        redirect("/");
+    }
+
     return (
         <div>
             <Appbar />
--- middleware.ts
+++ middleware.ts
@@ -4,32 +4,12 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
 
-const ALLOWED_ROLES = [
-    "FLOORSTAFF",
-    "TEAMLEADER",
-    "MANAGER",
-];
-
 export async function middleware(req: NextRequest) {
-    const token = await getToken({
-        req,
-        secret: process.env.NEXTAUTH_SECRET,
-    });
-
-    console.log("TOKEN", token);
-    console.log("ROLE", token?.role);
+    const token = await getToken({ req });
 
     if (!token) {
         return NextResponse.redirect(
-            new URL("/auth/login", req.url)
-        );
-    }
-
-    const role = token.role as string;
-
-    if (!ALLOWED_ROLES.includes(role)) {
-        return NextResponse.redirect(
-            new URL("/", req.url)
+            new URL("/login", req.url)
         );
     }
 
@@ -37,5 +17,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-    matcher: ["/manage/:path*"],
+    matcher: ["/manage", "/manage/:path*"],
 };
